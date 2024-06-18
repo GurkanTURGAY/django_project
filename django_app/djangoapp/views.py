2c3d32700744ab1cfa4a6d610f137162583cc2a2
Gurkan TURGAY <gurkan.turgay29@gmail.com>
2024-06-18 13:28:53 +0300

    urls & views update

--- django_app/djangoapp/views.py
+++ django_app/djangoapp/views.py
@@ -1,14 +1,33 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import redirect, render
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
 
-# Create your views here.
-
-# http://127.0.0.1:8000/        > anasayfa
-# http://127.0.0.1:8000/home    > anasayfa
-# http://127.0.0.1:8000/kurslar > kurslar
-
-def home(request):
-    return HttpResponse('anasayfa')
+data = {
+    "programlama" : "programlama kategorisine ait kurslar",
+    "web-gelistirme" : "web geliştirme kategorisine ait kurslar",
+    "mobil" : "mobil kategorisine ait kurslar",
+}
 
 def kurslar(request):
-    return HttpResponse('kurs listesi')
+    return HttpResponse("kurs listesi")
+
+def details(request, kurs_adi):
+    return HttpResponse(f"{kurs_adi} detay sayfası")
+
+def getCoursesByCategory(request, category_name):
+    try:
+        category_text = data[category_name]
+        return HttpResponse(category_text)
+    except:
+        return HttpResponseNotFound("yanlış kategori seçimi")
+    
+
+def getCoursesByCategoryId(request, category_id):
+    category_list = list(data.keys())
+    if (category_id > len(category_list)):
+        return HttpResponseNotFound("yanlış kategori seçimi")
+    
+    category_name = category_list[category_id - 1]
+
+    redirect_url = reverse('courses_by_category', args=[category_name])
+    return redirect(redirect_url)

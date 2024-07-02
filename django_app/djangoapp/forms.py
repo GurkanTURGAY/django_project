489a37782ef4319d1bb84899623304e07a5cb88e
Gurkan TURGAY <gurkan.turgay29@gmail.com>
2024-07-02 13:57:38 +0300

    course edit form

--- django_app/djangoapp/forms.py
+++ django_app/djangoapp/forms.py
@@ -1,5 +1,8 @@
 from django import forms
 
+from djangoapp.models import Course
+
+"""
 class CourseCreateForm(forms.Form):
     title = forms.CharField(label="Kurs Başlığı",
                             required=True,
@@ -23,4 +26,51 @@ class CourseCreateForm(forms.Form):
     slug = forms.SlugField(error_messages={
                                 "required":"Slug Alanı Girmelisiniz."
                             },
-                            widget= forms.TextInput(attrs={"class":"form-control"}))
+                            widget= forms.TextInput(attrs={"class":"form-control"}))"""
+
+class CourseCreateForm(forms.ModelForm):
+    class Meta:
+        model = Course
+        fields = ('title','description', 'imageUrl', 'slug')
+        labels = {
+            'title':"Kurs Başlığı",
+            'description':"Kurs Açıklaması"
+        }
+        widgets = {
+            "title":forms.TextInput(attrs={"class":"form-control"}),
+            "description":forms.Textarea(attrs={"class":"form-control"}),
+            "imageUrl":forms.TextInput(attrs={"class":"form-control"}),
+            "slug":forms.TextInput(attrs={"class":"form-control"})
+        }
+        error_messages = {
+            "title": {
+                "required": "Kurs Başlığı Girmelisiniz."
+            },
+            "description":{
+                "required": "Kurs Açıklaması Girmelisiniz."
+            }
+        }
+
+class CourseEditForm(forms.ModelForm):
+    class Meta:
+        model = Course
+        fields = ('title','description', 'imageUrl', 'slug','categories')
+        labels = {
+            'title':"Kurs Başlığı",
+            'description':"Kurs Açıklaması"
+        }
+        widgets = {
+            "title":forms.TextInput(attrs={"class":"form-control"}),
+            "description":forms.Textarea(attrs={"class":"form-control"}),
+            "imageUrl":forms.TextInput(attrs={"class":"form-control"}),
+            "slug":forms.TextInput(attrs={"class":"form-control"}),
+            "categories": forms.SelectMultiple(attrs={"class":"form-control"})
+        }
+        error_messages = {
+            "title": {
+                "required": "Kurs Başlığı Girmelisiniz."
+            },
+            "description":{
+                "required": "Kurs Açıklaması Girmelisiniz."
+            }
+        }

9a51b408c003053d45ec963af249da8d17fc4e41
Gurkan TURGAY <gurkan.turgay29@gmail.com>
2024-07-03 15:25:56 +0300

    user login page

--- django_app/django_app/urls.py
+++ django_app/django_app/urls.py
@@ -4,7 +4,8 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('kurslar/', include('djangoapp.urls')),
     path('', include('pages.urls')),
+    path('kurslar/', include('djangoapp.urls')),
+    path('account/', include('account.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
